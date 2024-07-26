cb8e0b007d8dc06ad0776b11348b56c5dd60c03a
FarukEww <shaikh.farukhmiya@excellentwebworld.in>
2024-07-26 14:52:20 +0530

    port change

--- index.ts
+++ index.ts
@@ -29,7 +29,7 @@ app.use(cookieParser());
 app.use(express.static("app/public"));
 app.use("/public", express.static(path.join(__dirname, "public")));
 // app.use(express.bodyParser())
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5473;
 connectDB();
 interface RequestWithGuestId extends Request {
   guestId?: string;
