28fbdb0905ae5684ecc3361a78e973130b15808b
Faruk Shaikh <146949375+FarukEww@users.noreply.github.com>
2024-08-20 13:54:23 +0530

    Delete server directory

--- server/routes/mainRotes.ts
+++
@@ -1,9 +0,0 @@
-import adminRouter from "./admin/adminRoutes";
-import express, { Router } from "express";
-import webRouter from "./web/webRoutes";
-const mainRouter: Router = express.Router();
-mainRouter.use("/ecom/api/website", webRouter);
-mainRouter.use("/ecom/api/admin", adminRouter);
-
-export default mainRouter;
-    
